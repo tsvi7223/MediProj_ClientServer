166d9474beeb989c47ea6cfb00054d5386643dfd
danielassouline <danielassouline@DANIELASSOUF9D1>
2026-06-21 21:58:54 +0300

    סידרתי את הקוד כך שאם אני מוסיף רכישת תרופות והתאריך לא יעקוף את היום אז התרופה תופיע רק בהיסטוריית תרופות ולא במתי לקנות תרופה

--- MediProject_Client/GUI/MedicationExpirationPage.xaml.cs
+++ MediProject_Client/GUI/MedicationExpirationPage.xaml.cs
@@ -1,4 +1,6 @@
 ﻿using MediProject_Client.ServiceReference1;
+using System;
+using System.Collections.Generic; // נדרש עבור ה-List
 using System.Windows.Controls;
 
 namespace MediProject_Client.GUI
@@ -17,9 +19,27 @@ namespace MediProject_Client.GUI
 
         private void LoadExpirationData()
         {
-            
+            // קבלת כל הרשימה מהשרת
             MedicationExpiration[] list = service.GetMedicationExpirationList(user);
-            medicationExpirationList.ItemsSource = list;
+
+            // יצירת רשימה חדשה לסינון
+            List<MedicationExpiration> filteredList = new List<MedicationExpiration>();
+
+            // מעבר על הרשימה כדי לסנן רק את מה שרלוונטי מהיום והלאה
+            if (list != null)
+            {
+                foreach (MedicationExpiration item in list)
+                {
+                    // תנאי: אם התאריך הוא היום או בעתיד - נוסיף לרשימה
+                    if (item.ExpirationDate >= DateTime.Today)
+                    {
+                        filteredList.Add(item);
+                    }
+                }
+            }
+
+            // עדכון ה-ItemsSource רק עם הרשימה המסוננת
+            medicationExpirationList.ItemsSource = filteredList;
         }
     }
 }
